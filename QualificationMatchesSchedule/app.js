e20a2652adc9d9e9cff659c618bc42879dbe9ca3
Ky Duyen <daonguyenk06@gmail.com>
2023-07-13 15:32:39 -0400

    add js code for qualMatchSchedule

--- QualificationMatchesSchedule/app.js
+++ QualificationMatchesSchedule/app.js
@@ -1,4 +1,53 @@
-const addRowButton = document.getElementById(addRowButton);
+const tbody = document.getElementById("tbody");
+const tr = document.getElementsByClassName("tr");
 
-let timeRow = document.getElementById(time);
-let matchRow = document.getElementById(matchNumber);
+
+let timeRow = document.getElementById("time");
+let matchRow = document.getElementById("matchNumber");
+let rowCount = 1;
+removeRow
+function addRow(){
+    tbody.innerHTML += `
+    <tr class="tr">
+        <td class = "time">
+            <input class="timeInput" id="timeInput_${rowCount}" type="time">
+        </td>
+        <td class = "matchVideo" id="matchVideo_${rowCount}">
+            <a href="">link</a>
+        </td>
+        <td class = "matchNumber">
+            <input class="matchInput" id="matchInput_${rowCount}" type="number">
+        </td>
+        <td class = "redAlliance" href="">
+            <input class="redInput" id="red1_${rowCount}" type="number">
+        </td>
+        <td class = "redAlliance">
+            <input class="redInput" id="red2_${rowCount}" type="number">
+        </td>
+        <td class = "redAlliance">
+            <input class="redInput" id="red3_${rowCount}" type="number">
+        </td>
+        <td class = "blueAlliance">
+            <input class="blueInput" id="blue1_${rowCount}" type="number">
+        </td>
+        <td class = "blueAlliance">
+            <input class="blueInput" id="blue2_${rowCount}" type="number">
+        </td>
+        <td class = "blueAlliance">
+            <input class="blueInput" id="blue3_${rowCount}" type="number">
+        </td>
+    </tr>`;
+    rowCount++;
+}
+
+function removeRow(){
+    if(tr.length > 1){
+        let currentChild = tr[tr.length - 1];
+        console.log(currentChild.parentNode.removeChild(currentChild));
+        rowCount--;
+    }
+}
+
+// function save() {
+//     let input = document.getElementsByClassName()
+// }
