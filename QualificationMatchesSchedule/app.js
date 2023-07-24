604745bff3c46f4fcca4569e490b53e17db6f9b1
Ky Duyen <daonguyenk06@gmail.com>
2023-07-23 19:55:42 -0500

    replaced rowCount to tr.length-1

--- QualificationMatchesSchedule/app.js
+++ QualificationMatchesSchedule/app.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
+import { getDatabase, ref, push, onValue, update, remove } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
 //ref(): to refer to the targeted database
 //push(): to save values to database
 //onValue(): to get values from database
@@ -30,6 +30,7 @@ const app = initializeApp(appSettings);
 const database = getDatabase(app);
 
 //Refering to each folders in database and store in variables
+const qualTable = ref(database, "qualSchedule/Table")
 const qualTime = ref(database, "qualSchedule/Time");
 const qualMatch = ref(database, "qualSchedule/Match");
 const qualRed1 = ref(database, "qualSchedule/Red1");
@@ -42,20 +43,19 @@ const qualBlue3 = ref(database, "qualSchedule/Blue3");
 const tbody = document.getElementById("tbody");
 const tr = document.getElementsByClassName("tr");
 const removeRowButton = document.getElementById("removeRowButton");
+const addRowButton = document.getElementById("addRowButton");
 const saveButton = document.getElementById("saveButton");
 
-let rowCount = 0;
 
-//TODO: access folders from outside path, and insert link
-//TODO: make sure table rows and rowCount are saved and printed after user exit
+//TODO: push tbody to database and make values show in desig place
+//TODO: add a delete function that deletes the whole table
 //TODO: if time >= localTime, then change color --see HTML JavaScript w3schools
 
 //Codes for saveButton: prints inputs to row below and add row
 saveButton.addEventListener("click", function() {
-    console.log("save_"+(rowCount));
+    console.log("save_"+(tr.length - 1));
 
     saveTime();
-    // saveLink();
     saveMatchNum();
     saveRed1();
     saveRed2();
@@ -65,15 +65,19 @@ saveButton.addEventListener("click", function() {
     saveBlue3();
 });
 
-//Codes for removeRowButton: removes last printed row
+//Codes for removeRowButton: removes last row
 removeRowButton.addEventListener("click", function() {
     if(tr.length > 1){
         let desigRow = tr[tr.length - 2];
         console.log(desigRow.parentNode.removeChild(desigRow));
-        //rowCount--; //Somehow leaving this out got it work, but row number kept going.
-        console.log("remove row"+(rowCount));
+        console.log("remove row"+(tr.length - 1));
     }
 });
+
+addRowButton.addEventListener("click", function() {
+    addEmptyRow();
+    console.log(tbody.innerHTML);
+})
 
 function saveTime() {
     let timeInput = document.getElementById("timeInput").value;
@@ -81,8 +85,7 @@ function saveTime() {
 
     onValue(qualTime, function(snapshot) {
     let timeArray = Object.values(snapshot.val());
-    console.log(timeArray[rowCount])
-    document.getElementById("timeInput_"+(rowCount)).innerHTML = timeArray[rowCount];
+    document.getElementById("timeInput_"+(tr.length - 1)).innerHTML = timeArray[tr.length - 1];
     });
 }
 
@@ -92,7 +95,7 @@ function saveMatchNum() {
 
     onValue(qualMatch, function(snapshot) {
     let matchArray = Object.values(snapshot.val());
-    document.getElementById("matchInput_"+(rowCount)).innerHTML = matchArray[rowCount];
+    document.getElementById("matchInput_"+(tr.length - 1)).innerHTML = matchArray[tr.length - 1];
     });
 }
 
@@ -102,7 +105,7 @@ function saveRed1() {
 
     onValue(qualRed1, function(snapshot) {
     let red1Array = Object.values(snapshot.val());
-    document.getElementById("red1_"+(rowCount)).innerHTML = red1Array[rowCount];
+    document.getElementById("red1_"+(tr.length - 1)).innerHTML = red1Array[tr.length - 1];
     });
     
 }
@@ -113,7 +116,7 @@ function saveRed2() {
 
     onValue(qualRed2, function(snapshot) {
     let red2Array = Object.values(snapshot.val());
-    document.getElementById("red2_"+(rowCount)).innerHTML = red2Array[rowCount];
+    document.getElementById("red2_"+(tr.length - 1)).innerHTML = red2Array[tr.length - 1];
     });
 }
 
@@ -123,7 +126,7 @@ function saveRed3() {
 
     onValue(qualRed3, function(snapshot) {
     let red3Array = Object.values(snapshot.val());
-    document.getElementById("red3_"+(rowCount)).innerHTML = red3Array[rowCount];
+    document.getElementById("red3_"+(tr.length - 1)).innerHTML = red3Array[tr.length - 1];
     });
 }
 
@@ -133,7 +136,7 @@ function saveBlue1() {
 
     onValue(qualBlue1, function(snapshot) {
     let blue1Array = Object.values(snapshot.val());
-    document.getElementById("blue1_"+(rowCount)).innerHTML = blue1Array[rowCount];
+    document.getElementById("blue1_"+(tr.length - 1)).innerHTML = blue1Array[tr.length - 1];
     });
 }
 
@@ -143,7 +146,7 @@ function saveBlue2() {
 
     onValue(qualBlue2, function(snapshot) {
     let blue2Array = Object.values(snapshot.val());
-    document.getElementById("blue2_"+(rowCount)).innerHTML = blue2Array[rowCount];
+    document.getElementById("blue2_"+(tr.length - 1)).innerHTML = blue2Array[tr.length - 1];
     });
 }
 
@@ -153,29 +156,28 @@ function saveBlue3() {
 
     onValue(qualBlue3, function(snapshot) {
     let blue3Array = Object.values(snapshot.val());
-    document.getElementById("blue3_"+(rowCount)).innerHTML = blue3Array[rowCount];
+    document.getElementById("blue3_"+(tr.length - 1)).innerHTML = blue3Array[tr.length - 1];
     });
 }
 
 function addEmptyRow() {
     tbody.innerHTML += `
     <tr class="tr">
-        <td class = "time" id="timeInput_${rowCount + 1}">
+        <td class = "time" id="timeInput_${tr.length}">
         </td>
-        <td class = "matchNumber" id="matchInput_${rowCount + 1}" >
+        <td class = "matchNumber" id="matchInput_${tr.length}" >
         </td>
-        <td class = "redAlliance" id="red1_${rowCount + 1}">
+        <td class = "redAlliance" id="red1_${tr.length}">
         </td>
-        <td class = "redAlliance" id="red2_${rowCount + 1}">
+        <td class = "redAlliance" id="red2_${tr.length}">
         </td>
-        <td class = "redAlliance" id="red3_${rowCount + 1}">
+        <td class = "redAlliance" id="red3_${tr.length}">
         </td>
-        <td class = "blueAlliance" id="blue1_${rowCount + 1}">
+        <td class = "blueAlliance" id="blue1_${tr.length}">
         </td>
-        <td class = "blueAlliance" id="blue2_${rowCount + 1}">
+        <td class = "blueAlliance" id="blue2_${tr.length}">
         </td>
-        <td class = "blueAlliance" id="blue3_${rowCount + 1}">
+        <td class = "blueAlliance" id="blue3_${tr.length}">
         </td>
-    </tr>`;  
-    rowCount++;
+    </tr>`;
 }
