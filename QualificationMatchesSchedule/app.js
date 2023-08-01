2867cd9bb09afbdc3387098129867e76b556d857
Ky Duyen <daonguyenk06@gmail.com>
2023-07-31 23:02:11 -0500

    Update app.js

--- QualificationMatchesSchedule/app.js
+++ QualificationMatchesSchedule/app.js
@@ -30,7 +30,7 @@ const app = initializeApp(appSettings);
 const database = getDatabase(app);
 
 //Refering to each folders in database and store in variables
-const qualTable = ref(database, "qualSchedule/Table")
+const qualRow = ref(database, "qualSchedule/Row")
 const qualTime = ref(database, "qualSchedule/Time");
 const qualMatch = ref(database, "qualSchedule/Match");
 const qualRed1 = ref(database, "qualSchedule/Red1");
@@ -61,12 +61,14 @@ const blue3Input = document.getElementById("blue3");
 
 
 //TODO: make sure removeRow removes only one row without having to refresh page
-//Substitution: add edit function designated to each row so we wouldn't have to remove row
+//TODO: add edit function designated to each row so we wouldn't have to remove row
 
 //TODO: separate all firebase code in a different file
-//TODO: add a delete function that deletes the whole table
 //TODO: if time >= localTime, then change color --see HTML JavaScript w3schools
 //TODO: (optional) add hover effects --see firebase mobile app tutorial
+
+
+updateTable(); //Get saved data as soon as page open
 
 //Push input to database and add row
 saveButton.addEventListener("click", function() {
@@ -82,86 +84,82 @@ saveButton.addEventListener("click", function() {
     
     addEmptyRow(); //Adds empty row for new inputs that are programmed to display immediately
     resetInputFields(); //Empty input fields after inputs are saved.
+    updateTable();
     console.log("save_" + (tr.length - 1));
 });
 
 //removes last row (supposedly)
 removeRowButton.addEventListener("click", function() {
     removeLastRow();
+    // updateTable();
 });
 
 //resets the whole table
 resetButton.addEventListener("dblclick", function() {
     reset();
+    updateTable();
 });
+
+/*** Below are only for functions ***/
 
 //Get saved data from database
-onValue(qualTable, function(snapshot) {
-let rowArray = Object.values(snapshot.val());
-tbody.innerHTML = rowArray.join(""); //join("") replaces "," to "" between values in rowArray
+function updateTable() {
+    onValue(qualRow, function(snapshot) {
+    let rowArray = Object.values(snapshot.val());
+    tbody.innerHTML = rowArray.join(""); //join("") replaces "," to "" between values in rowArray
 
-    for(let i = 0; i < tr.length; i++) {
-        onValue(qualTime, function(snapshot) {
-        let timeArray = Object.values(snapshot.val());
-        document.getElementById("timeInput_"+(i)).innerHTML = timeArray[i];
-        console.log(timeArray[i]); 
-        });
-    
-        onValue(qualMatch, function(snapshot) {
-        let matchArray = Object.values(snapshot.val());
-        document.getElementById("matchInput_"+(i)).innerHTML = matchArray[i];
-        console.log(matchArray[i]);
-        });
-    
-        onValue(qualRed1, function(snapshot) {
-        let red1Array = Object.values(snapshot.val());
-        document.getElementById("red1_"+(i)).innerHTML = red1Array[i];
-        console.log(red1Array[i]);
-        });
+        for(let i = 0; i < tr.length; i++) {
+            onValue(qualTime, function(snapshot) {
+            let timeArray = Object.values(snapshot.val());
+            document.getElementById("timeInput_"+(i)).innerHTML = timeArray[i];
+            console.log(timeArray[i]); 
+            });
         
-        onValue(qualRed2, function(snapshot) {
-        let red2Array = Object.values(snapshot.val());
-        document.getElementById("red2_"+(i)).innerHTML = red2Array[i];
-        console.log(red2Array[i]);
-        });
+            onValue(qualMatch, function(snapshot) {
+            let matchArray = Object.values(snapshot.val());
+            document.getElementById("matchInput_"+(i)).innerHTML = matchArray[i];
+            console.log(matchArray[i]);
+            });
         
-        onValue(qualRed3, function(snapshot) {
-        let red3Array = Object.values(snapshot.val());
-        document.getElementById("red3_"+(i)).innerHTML = red3Array[i];
-        console.log(red3Array[i]);
-        });
-    
-        onValue(qualBlue1, function(snapshot) {
-        let blue1Array = Object.values(snapshot.val());
-        document.getElementById("blue1_"+(i)).innerHTML = blue1Array[i];
-        console.log(blue1Array[i]);
-        });
-    
-        onValue(qualBlue2, function(snapshot) {
-        let blue2Array = Object.values(snapshot.val());
-        document.getElementById("blue2_"+(i)).innerHTML = blue2Array[i];
-        console.log(blue2Array[i]);
-        });
+            onValue(qualRed1, function(snapshot) {
+            let red1Array = Object.values(snapshot.val());
+            document.getElementById("red1_"+(i)).innerHTML = red1Array[i];
+            console.log(red1Array[i]);
+            });
+            
+            onValue(qualRed2, function(snapshot) {
+            let red2Array = Object.values(snapshot.val());
+            document.getElementById("red2_"+(i)).innerHTML = red2Array[i];
+            console.log(red2Array[i]);
+            });
+            
+            onValue(qualRed3, function(snapshot) {
+            let red3Array = Object.values(snapshot.val());
+            document.getElementById("red3_"+(i)).innerHTML = red3Array[i];
+            console.log(red3Array[i]);
+            });
         
-        onValue(qualBlue3, function(snapshot) {
-        let blue3Array = Object.values(snapshot.val());
-        document.getElementById("blue3_"+(i)).innerHTML = blue3Array[i];
-        console.log(blue3Array[i]);
-        });
-    }
-});
+            onValue(qualBlue1, function(snapshot) {
+            let blue1Array = Object.values(snapshot.val());
+            document.getElementById("blue1_"+(i)).innerHTML = blue1Array[i];
+            console.log(blue1Array[i]);
+            });
+        
+            onValue(qualBlue2, function(snapshot) {
+            let blue2Array = Object.values(snapshot.val());
+            document.getElementById("blue2_"+(i)).innerHTML = blue2Array[i];
+            console.log(blue2Array[i]);
+            });
+            
+            onValue(qualBlue3, function(snapshot) {
+            let blue3Array = Object.values(snapshot.val());
+            document.getElementById("blue3_"+(i)).innerHTML = blue3Array[i];
+            console.log(blue3Array[i]);
+            });
+        }
+    });
+}
 
-
-onValue(qualTable, function(snapshot) {
-    let rowIDs = Object.keys(snapshot.val());
-    let lastRowID = rowIDs[rowIDs.length - 1]; //Gets the ID of last row
-    let lastRow = ref(database, `qualSchedule/Table/${lastRowID}`); //Refers to last row
-    console.log(lastRow);
-});
-
-
-
-//Below are only for functions
 function saveTime() {
     let timeInput = document.getElementById("timeInput").value;
     push(qualTime, timeInput);
@@ -222,7 +220,7 @@ function addEmptyRow() {
         </td>
     </tr>`;
     tbody.innerHTML += newRow;
-    push(qualTable, newRow);
+    push(qualRow, newRow);
 }
 
 function resetInputFields() {
@@ -240,87 +238,88 @@ function resetInputFields() {
 
 function removeLastRow() {
     
-    onValue(qualTable, function(snapshot) {
-        let rowIDs = Object.keys(snapshot.val());
-        let lastRowID = rowIDs[rowIDs.length - 1]; //Gets the ID of last row
-        let lastRow = ref(database, `qualSchedule/Table/${lastRowID}`); //Refers to last row
-
-            if(rowIDs.length > 0) {
-                remove(lastRow); //Removes last value from table on database
-            }
-    });
+    // onValue(qualRow, function(snapshot) {
+    //     let rowIDs = Object.keys(snapshot.val());
+    //     let lastRowID = rowIDs[rowIDs.length - 1]; //Gets the ID of last row
+    //     let lastRow = ref(database, `qualSchedule/Table/${lastRowID}`); //Refers to last row
+    //         if(rowIDs.length > 0) {
+    //             remove(lastRow); //Removes last value from table on database
+    //         }
+    // });
             
     onValue(qualTime, function(snapshot) {
         let timeIDs = Object.keys(snapshot.val());
         let lastTimeID = timeIDs[timeIDs.length - 1]; //Gets the timeID of last row
         let lastTime = ref(database, `qualSchedule/Time/${lastTimeID}`); //Refers to time of last row
-
             if(timeIDs.length > 0) {
                 remove(lastTime); //Removes last value from time on database
             }
+    });
             
+    onValue(qualMatch, function(snapshot) {
+        let matchIDs = Object.keys(snapshot.val());
+        let lastMatchID = matchIDs[matchIDs.length - 1]; //Gets the matchID of last row
+        let lastMatch = ref(database, `qualSchedule/Match/${lastMatchID}`); //Refers to match of last row
+            if(matchIDs.length > 0) {
+                remove(lastMatch); //Removes last value from matches on database
+            }
     });
 
-            //Removes last value from matches on database
-            onValue(qualMatch, function(snapshot) {
-            let matchIDs = Object.keys(snapshot.val());
-            let lastMatchID = matchIDs[rowArray.length - 1]; //Gets the matchID of last row
-            let lastMatch = ref(database, `qualSchedule/Match/${lastMatchID}`); //Refers to match of last row
-            remove(lastMatch);
-            });
+    onValue(qualRed1, function(snapshot) {
+        let red1IDs = Object.keys(snapshot.val());
+        let lastRed1ID = red1IDs[red1IDs.length - 1]; //Gets the red1ID of last row
+        let lastRed1 = ref(database, `qualSchedule/Red1/${lastRed1ID}`); //Refers to red1 of last row
+            if(red1IDs.length > 0) {
+                remove(lastRed1); //Removes last value from red1 on database
+            }
+    });
 
-            //Removes last value from red1 on database
-            onValue(qualRed1, function(snapshot) {
-            let red1IDs = Object.keys(snapshot.val());
-            let lastRed1ID = red1IDs[rowArray.length - 1]; //Gets the red1ID of last row
-            let lastRed1 = ref(database, `qualSchedule/Red1/${lastRed1ID}`); //Refers to red1 of last row
-            remove(lastRed1);
-            });
+    onValue(qualRed2, function(snapshot) {
+        let red2IDs = Object.keys(snapshot.val());
+        let lastRed2ID = red2IDs[red2IDs.length - 1]; //Gets the red2ID of last row
+        let lastRed2 = ref(database, `qualSchedule/Red2/${lastRed2ID}`); //Refers to red2 of last row
+            if(red2IDs.length > 0 ) {
+                remove(lastRed2); //Removes last value from red2 on database
+            }
+    });
+   
+    onValue(qualRed3, function(snapshot) {
+        let red3IDs = Object.keys(snapshot.val());
+        let lastRed3ID = red3IDs[red3IDs.length - 1]; //Gets the red3ID of last row
+        let lastRed3 = ref(database, `qualSchedule/Red3/${lastRed3ID}`); //Refers to red3 of last row
+            if(red3IDs.length > 0) {
+                remove(lastRed3); //Removes last value from red3 on database
+            }
+    });
+            
+    onValue(qualBlue1, function(snapshot) {
+        let blue1IDs = Object.keys(snapshot.val());
+        let lastBlue1ID = blue1IDs[blue1IDs.length - 1]; //Gets the blue1ID of last row
+        let lastBlue1 = ref(database, `qualSchedule/Blue1/${lastBlue1ID}`); //Refers to blue1 of last row
+            if(blue1IDs.length > 0) {
+                remove(lastBlue1); //Removes last value from blue1 on database
+            }
+    });
 
-            //Removes last value from red2 on database
-            onValue(qualRed2, function(snapshot) {
-            let red2IDs = Object.keys(snapshot.val());
-            let lastRed2ID = red2IDs[rowArray.length - 1]; //Gets the red2ID of last row
-            let lastRed2 = ref(database, `qualSchedule/Red2/${lastRed2ID}`); //Refers to red2 of last row
-            remove(lastRed2);
-            });
+    onValue(qualBlue2, function(snapshot) {
+        let blue2IDs = Object.keys(snapshot.val());
+        let lastBlue2ID = blue2IDs[blue2IDs.length - 1]; //Gets the blue2ID of last row
+        let lastBlue2 = ref(database, `qualSchedule/Blue2/${lastBlue2ID}`); //Refers to blue2 of last row
+            if(blue2IDs.length > 0) {
+                remove(lastBlue2); //Removes last value from blue2 on database
+            }
+    });
 
-            //Removes last value from red3 on database
-            onValue(qualRed3, function(snapshot) {
-            let red3IDs = Object.keys(snapshot.val());
-            let lastRed3ID = red3IDs[rowArray.length - 1]; //Gets the red3ID of last row
-            let lastRed3 = ref(database, `qualSchedule/Red3/${lastRed3ID}`); //Refers to red3 of last row
-            remove(lastRed3);
-            });
-
-            //Removes last value from blue1 on database
-            onValue(qualBlue1, function(snapshot) {
-            let blue1IDs = Object.keys(snapshot.val());
-            let lastBlue1ID = blue1IDs[rowArray.length - 1]; //Gets the blue1ID of last row
-            let lastBlue1 = ref(database, `qualSchedule/Blue1/${lastBlue1ID}`); //Refers to blue1 of last row
-            remove(lastBlue1);
-            });
-
-            //Removes last value from blue2 on database
-            onValue(qualBlue2, function(snapshot) {
-            let blue2IDs = Object.keys(snapshot.val());
-            let lastBlue2ID = blue2IDs[rowArray.length - 1]; //Gets the blue2ID of last row
-            let lastBlue2 = ref(database, `qualSchedule/Blue2/${lastBlue2ID}`); //Refers to blue2 of last row
-            remove(lastBlue2);
-            });
-
-            //Removes last value from blue3 on database
-            onValue(qualBlue3, function(snapshot) {
-            let blue3IDs = Object.keys(snapshot.val());
-            let lastBlue3ID = blue3IDs[rowArray.length - 1]; //Gets the blue3ID of last row
-            let lastBlue3 = ref(database, `qualSchedule/Blue3/${lastBlue3ID}`); //Refers to blue3 of last row
-            remove(lastBlue3);
-            });
-            location.reload(); //This is a temporary solution for not deleting multiple rows at once.
+    onValue(qualBlue3, function(snapshot) {
+        let blue3IDs = Object.keys(snapshot.val());
+        let lastBlue3ID = blue3IDs[blue3IDs.length - 1]; //Gets the blue3ID of last row
+        let lastBlue3 = ref(database, `qualSchedule/Blue3/${lastBlue3ID}`); //Refers to blue3 of last row
+            if(blue3IDs.length > 0) {
+                remove(lastBlue3); //Removes last value from blue3 on database
+            }
+    });
 }
 
 function reset() {
-    let table = ref(database, "qualSchedule");
-    remove(table);
-    location.reload();
+    remove(qualRow, qualTime, qualMatch, qualRed1, qualRed2, qualRed3, qualBlue1, qualBlue2, qualBlue3);
 }
